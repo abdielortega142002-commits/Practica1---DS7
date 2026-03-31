1b007f7516006acdca039acbf102b71ab3cdd05f
Abdiel Ortega <abdielortega142002@gmail.com>
2026-03-31 09:34:28 -0500

    agregando ejemplo#2.php

---
+++ ejemplo#2.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <title>Ejemplo de  variables</title>
+</head>
+<body>
+    <h1>
+        Ejemplo de Operaciones aritmeticas
+    </h1>
+    <?php
+    //suma
+    $numero1 = 5;
+    $numero2 = 3;
+    $resultado = $numero1 + $numero2;
+    echo "El resultado de la suma es: $resultado";
+    //resta
+    $numero1 = 10;
+    $numero2 = 4;
+    $resultado = $numero1 - $numero2;
+    echo "<br>El resultado de la resta es: $resultado";
+    //multiplicación
+    $numero1 = 7;
+    $numero2 = 3;
+    $resultado = $numero1 * $numero2;
+    echo "<br>El resultado de la multiplicación es: $resultado";
+    //division
+    $numero1 = 15;
+    $numero2 = 5;
+    $resultado = $numero1 / $numero2;
+    echo "<br>El resultado de la división es: $resultado";
+    //modulo
+    $numero1 = 10;
+    $numero2 = 3;
+    $resultado = $numero1 % $numero2;
+    echo "<br>El resultado del módulo es: $resultado";
+    //potencia
+    $base = 2;
+    $exponente = 3;
+    $resultado = $base ** $exponente;
+    echo "<br>El resultado de la potencia es: $resultado";
+    ?>
+</body>
+</html>
