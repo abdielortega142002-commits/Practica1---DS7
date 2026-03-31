5d8b963be0c26c6a1b2fb1c633f8aadd8bb5bd48
Abdiel Ortega <abdielortega142002@gmail.com>
2026-03-31 09:35:12 -0500

    agregando ejemplo#3.php

---
+++ ejemplo#3.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Ejemplo de  variables</title>
+</head>
+<body>
+    <h1>
+        Ejemplo de orden de precendencia
+    </h1>
+    <?php
+    $resultado = 2 + 3 * 4;
+    echo "El resultado de 2 + 3 * 4 es: $resultado";
+    $resultado = (2 + 3) * 4;
+    echo "<br>El resultado de (2 + 3) * 4 es: $resultado";
+    ?>
+</body>
+</html>
